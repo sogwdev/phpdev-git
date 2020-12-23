a104c910881307ce644ab70fbe3676540dc7790d
sogwdev <ogwdev01@gmail.com>
2020-12-23 23:04:41 +0900

    zzzz1

--- user.php
+++ user.php
@@ -7,10 +7,10 @@
 <?php
 
 $user=htmlspecialchars($_POST["u"],ENT_QUOTES);
-<h2>$userの投稿</h2>
+print "$userの投稿"
 
 $db = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;port=3306;dbname=heroku_ef5f71273fe5d40;charset=utf8','b712f8e8641bf4','466f991d');
-$ps = $db->query("SELECT * FROM admin where name=$user");
+$ps = $db->query("SELECT * FROM admin where name='$user'");
 while ($r = $ps->fetch()){
 print "{$r['name']} {$r['msg']} {$r['dt']}<hr>";}
 ?>
